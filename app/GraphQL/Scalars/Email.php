ca85e71ed3b24ed654dc711e67925edef095fb66
nielsklazenga <Niels.Klazenga@rbg.vic.gov.au>
2021-03-16 23:21:01 +1100

    Create Email scalar

---
+++ app/GraphQL/Scalars/Email.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\GraphQL\Scalars;
+
+use GraphQL\Error\Error;
+use GraphQL\Type\Definition\ScalarType;
+use GraphQL\Utils\Utils;
+
+/**
+ * Read more about scalars here https://webonyx.github.io/graphql-php/type-system/scalar-types/
+ */
+class Email extends ScalarType
+{
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        // Assuming the internal representation of the value is always correct
+        return $value;
+
+        // TODO validate if it might be incorrect
+    }
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function parseValue($value)
+    {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            throw new Error("Cannot represent following value as email: " . Utils::printSafeJson($value));
+        }
+        return $value;
+
+        return $value;
+    }
+
+    /**
+     * Parses an externally provided literal value (hardcoded in GraphQL query) to use as an input.
+     *
+     * E.g.
+     * {
+     *   user(email: "user@example.com")
+     * }
+     *
+     * @param  \GraphQL\Language\AST\Node  $valueNode
+     * @param  array<string, mixed>|null  $variables
+     * @return mixed
+     */
+    public function parseLiteral($valueNode, ?array $variables = null)
+    {
+        if (!$valueNode instanceof StringValueNode) {
+            throw new Error('Query error: Can only parse strings got: ' . $valueNode->kind, [$valueNode]);
+        }
+        if (!filter_var($valueNode->value, FILTER_VALIDATE_EMAIL)) {
+            throw new Error("Not a valid email", [$valueNode]);
+        }
+        return $valueNode->value;
+    }
+}
