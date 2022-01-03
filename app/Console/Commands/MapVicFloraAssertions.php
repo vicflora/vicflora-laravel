e823950b5c49c4e581d970b6b8670fb507ebc819
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-01-03 23:27:29 +1100

    [mapper] Add VicFlora assertions

---
+++ app/Console/Commands/MapVicFloraAssertions.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Assertion;
+use App\Models\Occurrence;
+use App\Models\TermValue;
+use Illuminate\Console\Command;
+
+class MapVicFloraAssertions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mapper:map-vicflora-assertions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import VicFlora assertions, map catalogue number to new UUID';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $agent_map = [
+            1 => 1,
+            2 => 2,
+            3 => 3,
+            7 => 4,
+            8 => 5
+        ];
+
+        $notFound = fopen(storage_path('app/assertions_vicflora_catalog_numbers_not found.csv'), 'w');
+
+        $handle = fopen(storage_path('app/vicflora_assertions.csv'), 'r');
+        $firstRow = fgetcsv($handle);
+        while (!feof($handle)) {
+
+            $row = [];
+            $line = fgetcsv($handle);
+            if ($line) {
+                foreach ($line as $key => $value) {
+                    $row[$firstRow[$key]] = $value ?: null;
+                }
+                if (!is_numeric($row['catalog_number'])) {
+
+                    // $occurrence = Occurrence::where('catalog_number', $row['catalog_number'])->first();
+                    $occurrence = Occurrence::whereRaw("replace(catalog_number, ' ', '')='" . str_replace(' ', '', $row['catalog_number']) . "'")
+                            ->first();
+
+                    if ($occurrence) {
+                        $this->info($occurrence->uuid);
+                        $term_value = TermValue::where('value', $row['asserted_value'])->first();
+
+                        Assertion::create([
+                            'occurrence_id' => $occurrence->uuid,
+                            'term_id' => $term_value->term_id,
+                            'term_value_id' => $term_value->id,
+                            'reason' => $row['reason'],
+                            'remarks' => $row['comment'],
+                            'created_at' => $row['timestamp_modified'],
+                            'updated_at' => $row['timestamp_modified'],
+                            'agent_id' => isset($row['user_id']) ? $agent_map[$row['user_id']] : null,
+                            'assertion_source_id' => 1
+                        ]);
+                    }
+                    else {
+                        $this->info('Not found: ' . $row['catalog_number']);
+                        fputcsv($notFound, [$row['catalog_number']]);
+                    }
+
+                }
+            }
+        }
+        fclose($handle);
+        fclose($notFound);
+    }
+}
