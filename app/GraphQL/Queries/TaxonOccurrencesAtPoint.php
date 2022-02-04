00fcc4de2dae020ffaa8660df57eb280f54718b8
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-02-04 16:39:36 +1100

    Use accepted_name_usage_id as key instead of taxon_concept_id in taxonOccurrencesAtPoint query

--- app/GraphQL/Queries/TaxonOccurrencesAtPoint.php
+++ app/GraphQL/Queries/TaxonOccurrencesAtPoint.php
@@ -12,7 +12,7 @@ class TaxonOccurrencesAtPoint
      */
     public function __invoke($_, array $args)
     {
-        return TaxonOccurrence::where('taxon_concept_id', $args['taxonConceptId'])
+        return TaxonOccurrence::where('accepted_name_usage_id', $args['taxonConceptId'])
                 ->whereRaw("ST_Dwithin(geom, ST_GeomFromText('POINT($args[longitude] $args[latitude])', 4326), 0.08)");
     }
 }
