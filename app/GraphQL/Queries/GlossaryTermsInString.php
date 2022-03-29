a56951aa81c0923fc398bcaab7b3056db10ca4d8
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-03-29 11:15:46 +1100

    Create glossary term in string type and query

---
+++ app/GraphQL/Queries/GlossaryTermsInString.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\GlossaryTerm;
+use Illuminate\Support\Facades\DB;
+
+class GlossaryTermsInString
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args): ?array
+    {
+        $ret = null;
+        preg_match_all('/\b[\w-]{3,}\b/', $args['string'], $matches);
+        if ($matches[0]) {
+            $values = [];
+            foreach ($matches[0] as $value) {
+                $values[] = "'$value'";
+            }
+            $values = implode(',', $values);
+
+            $sql = <<<SQL
+SELECT item, t.id 
+from terms t
+join unnest(array[$values]) as item
+    on t.name=lower(item) or t.name||'s'=lower(item)
+where t.glossary_id = 4
+SQL;
+
+            $result = DB::connection('glossary')->select($sql);
+
+            $ids = collect($result)->map(function ($item) {
+                return $item->id;
+            });
+
+            $terms = GlossaryTerm::whereIn('id', $ids)->get();
+
+            foreach ($result as $row) {
+                $ret[] = [
+                    'substring' => $row->item,
+                    'term' => $terms->first(function ($term, $key) use ($row) {
+                        return $term->id == $row->id;
+                    }),
+                ];
+            }
+        }
+        return $ret ?: null;
+    }
+}
