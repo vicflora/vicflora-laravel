768b97495aa8dc323711bdd08e2090fe68c7d5ff
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-11-02 10:39:35 +1100

    fix TaxonOccurrencesAtPoint query resolver
    - we now use taxon_concept_id rather than accepted_name_usage_id

--- app/GraphQL/Queries/TaxonOccurrencesAtPoint.php
+++ app/GraphQL/Queries/TaxonOccurrencesAtPoint.php
@@ -12,7 +12,7 @@ class TaxonOccurrencesAtPoint
      */
     public function __invoke($_, array $args)
     {
-        return TaxonOccurrence::where('accepted_name_usage_id', $args['taxonConceptId'])
+        return TaxonOccurrence::where('taxon_concept_id', $args['taxonConceptId'])
                 ->whereRaw("ST_Dwithin(geom, ST_GeomFromText('POINT($args[longitude] $args[latitude])', 4326), $args[distance])")
                 ->orderBy('data_source');
     }
