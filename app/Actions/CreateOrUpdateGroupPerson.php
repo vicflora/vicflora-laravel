c61876dcc89485c7b81991fa12e2365694fe6b72
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2022-12-17 09:56:32 +1100

    update CreateOrUpdateGroupPerson action
    - remove guid for group person, as it is not in the table

--- app/Actions/CreateOrUpdateGroupPerson.php
+++ app/Actions/CreateOrUpdateGroupPerson.php
@@ -1,12 +1,12 @@
 <?php
 // Copyright 2022 Royal Botanic Gardens Board
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class CreateOrUpdateGroupPerson {
-    
+
     public function __invoke($groupId, $member)
     {
         if (isset($member['id']) && $member['id']) {
@@ -31,13 +31,12 @@ class CreateOrUpdateGroupPerson {
         }
         else {
             $groupPerson = new GroupPerson();
-            $groupPerson->guid = Str::uuid();
             $groupPerson->created_by_id = Agent::where('user_id', Auth::id())->value('id');
             $groupPerson->version += 1;
         }
         $groupPerson->sequence = $member['sequence'];
         $groupPerson->group_id = $groupId;
-        $groupPerson->member_id = 
+        $groupPerson->member_id =
                 Agent::where('guid', $member['member']['connect'])->value('id');
         $groupPerson->save();
         return 0;
