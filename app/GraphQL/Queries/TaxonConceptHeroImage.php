4c00d87e78dbb4c5544dbd26eab10e98542eef7b
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-02-17 05:20:44 +0000

    update app/GraphQL/Queries/TaxonConceptHeroImage.php

--- app/GraphQL/Queries/TaxonConceptHeroImage.php
+++ app/GraphQL/Queries/TaxonConceptHeroImage.php
@@ -2,20 +2,41 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Models\Image;
 use App\Models\TaxonConcept;
-use App\Services\CantoImageService;
+use Illuminate\Support\Facades\DB;
 
 class TaxonConceptHeroImage
 {
     /**
-     * @param \App\Models\TaxonConcept $taxonConcept
-     * @param array $args
-     * @return array|null
+     * @param  TaxonConcept|null  $taxonConcept
+     * @param  array<string, mixed>  $args
      */
-    public function __invoke(TaxonConcept $taxonConcept, array $args): ?array
+    public function __invoke(TaxonConcept $taxonConcept, array $args)
     {
-        $imageService = new CantoImageService();
+        $query = TaxonConcept::where('id', $taxonConcept->id)
+        ->union(
+            TaxonConcept::select('taxon_concepts.*')
+                ->join('descendants', 'descendants.id', '=',
+                        'taxon_concepts.parent_id')
+        );
 
-        return $imageService->heroImage($taxonConcept->guid);
+        $descendants = TaxonConcept::from('descendants')
+                ->withRecursiveExpression('descendants', $query);
+        return Image::from('images')
+                ->join('taxon_concept_image', 'images.id', '=',
+                        'taxon_concept_image.image_id')
+                ->join('taxon_concepts', 'taxon_concept_image.taxon_concept_id',
+                        '=', 'taxon_concepts.id')
+                ->joinSub($descendants, 'descendants', function($join) {
+                    $join->on('taxon_concepts.accepted_id', '=',
+                            'descendants.id');
+                })
+                ->where('pixel_x_dimension', '>', 0)
+                ->orderBy('hero_image', 'asc')
+                ->orderBy('subtype', 'desc')
+                ->orderBy('rating', 'desc')
+                ->orderBy(DB::raw('random()'))
+                ->first();
     }
 }
