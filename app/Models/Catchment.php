165310e228198af97e8e8c20ccfa6e52c37a06ec
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2026-08-09 05:55:42 +0000

    Create Catchment model

---
+++ app/Models/Catchment.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Clickbar\Magellan\Database\Eloquent\HasPostgisColumns;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Catchment extends Model
+{
+    use HasPostgisColumns;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'mapper_overlays.catchments';
+
+    protected array $postgisColumns = [
+        'geom' => [
+            'type' => 'geometry',
+            'srid' => 4326,
+        ],
+    ];
+
+    public function getTypeAttribute()
+    {
+        return 'Feature';
+    }
+
+    /**
+     * GeoJSON 'geometry' object
+     *
+     * @return array<mixed>
+     */
+    public function getGeometryAttribute()
+    {
+        return [
+            'type' => 'MultiPolygon',
+            'coordinates' => json_decode($this->geom->toJson(), true)['coordinates'],
+        ];
+    }
+
+
+    public function getPropertiesAttribute()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->area_name,
+            'code' => $this->area_code,
+            'state' => 'Victoria',
+            'slug' => Str::slug($this->area_name),
+        ];
+    }
+}
